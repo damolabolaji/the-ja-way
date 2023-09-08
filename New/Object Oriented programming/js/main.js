b3102e4afd4e3aaad39fb2cfc6ba26345363bd35
damolabolaji <bolaji.oyindamola@gmail.com>
2023-09-08 16:24:31 +0100

    creates Dog class

--- New/Object Oriented programming/js/main.js
+++ New/Object Oriented programming/js/main.js
@@ -81,3 +81,31 @@ const monster = new Character("bobo", 50, 20);
 
 monster.attack(ladida);
 ladida.attack(monster);
+
+console.log(ladida.describe());
+console.log(monster.describe());
+
+//ADD DOG CONSTRUCTOR
+// TODO: define the Dog class here
+
+class Dog {
+  constructor(name, species, size) {
+    this.name = name;
+    this.species = species;
+    this.size = size;
+  }
+
+  bark() {
+    if (this.size > 60) {
+      return `GRR Grr`;
+    } else return `woof, woof`;
+  }
+}
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
